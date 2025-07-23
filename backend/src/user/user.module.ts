3040a5fdf18d89283fbb57c74e271e17edcf0a1f
Dev Arora <devarora1309@gmail.com>
2025-07-23 08:55:36 +0530

    Add JWT authentication setup to User module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,9 +3,16 @@ import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UserController } from './user.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'your_super_secret',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
